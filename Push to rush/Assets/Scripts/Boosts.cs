d1063efd8199848e61ce905c6aa233123537c18d
omar39 <45782982+omar39@users.noreply.github.com>
2019-08-04 14:18:08 +0200

    updates

--- Push to rush/Assets/Scripts/Boosts.cs
+++ Push to rush/Assets/Scripts/Boosts.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Boosts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject effect;
+    GameObject temp;
+   void OnTriggerEnter2D(Collider other)
     {
-        
+        temp = Instantiate(effect, transform);
+        StartCoroutine(Coroutine(other));
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator Coroutine(Collider player)
     {
-        
+        Destroy(GetComponentInChildren(typeof(GameObject)));    //destroy particle system
+        GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(1.5f);
+        Destroy(temp);
+        Destroy(gameObject);
     }
 }
