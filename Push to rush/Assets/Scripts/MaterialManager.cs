a7697aea99975cfcb71d79297089dc8fd64bd017
omar39 <45782982+omar39@users.noreply.github.com>
2019-08-04 14:17:10 +0200

    Update MaterialManager.cs

--- Push to rush/Assets/Scripts/MaterialManager.cs
+++ Push to rush/Assets/Scripts/MaterialManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class MaterialManager : MonoBehaviour
 {
     public GameObject effect;
+    public float increasePercent = 1.3f;
     GameObject temp;
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -13,7 +14,7 @@ public class MaterialManager : MonoBehaviour
     IEnumerator Coroutine(Collider2D player)
     {
         GetComponent<SpriteRenderer>().enabled = false;
-        player.gameObject.GetComponent<BotManager>().health *= 1.3f;
+        player.gameObject.GetComponent<BotManager>().health *= increasePercent;
         yield return new WaitForSeconds(1.3f);
         Destroy(temp);
         Destroy(gameObject);
