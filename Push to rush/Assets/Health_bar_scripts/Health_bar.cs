e80a2d31987a1d1ab9816eebd83a7e0e5531eb12
HeebaGamal <45473344+HeebaGamal@users.noreply.github.com>
2019-08-04 18:40:00 +0200

    Heeba
    conves heath bar
    
    Health_bar_scripts(Health_bar script added in health_bar image , script_trigger added in objects is trigger)
    
    Health_bar_Sprites

---
+++ Push to rush/Assets/Health_bar_scripts/Health_bar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health_bar : MonoBehaviour
+{
+    Image health_bar;
+    float max_health = 100f;
+    public static float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health_bar = GetComponent<Image>();
+        health = max_health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health_bar.fillAmount = health / max_health;
+    }
+}
---
+++ Push to rush/Assets/Health_bar_scripts/script_trigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_trigger : MonoBehaviour
+{
+   void OnTriggerEnter2D(Collider2D c)
+    {
+        Health_bar.health -= 10f;
+    }
+}
